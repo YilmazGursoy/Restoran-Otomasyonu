5413342498b27431419f7887aaf3f86741368c84
YilmazGursoy <ylmzgrsy111@gmail.com>
2016-05-06 14:14:41 +0300

    Initial Commit

---
+++ Restoran Otomasyonu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Restoran Otomasyonu
+//
+//  Created by Most Wanted on 06/05/16.
+//  Copyright © 2016 YilmazGursoy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Restoran Otomasyonu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Restoran Otomasyonu
+//
+//  Created by Most Wanted on 06/05/16.
+//  Copyright © 2016 YilmazGursoy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
